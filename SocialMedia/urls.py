28a7b46b3a7d6ddd4389aeb5081aede1fc4f4d6c
RakeshMuthineni <rakeshmuthineni9@gmail.com>
2021-10-28 11:44:12 +0530

    first commit

---
+++ SocialMedia/urls.py
@@ -0,0 +1,45 @@
+"""SocialMedia URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+from django.conf import settings
+
+
+urlpatterns = [
+    path('', views.HomePage.as_view(), name="home"),
+    path('admin/', admin.site.urls),
+
+    path('Mediaapp/', include("Mediaapp.urls", namespace="Mediaapp")),
+    path('Mediaapp/', include("django.contrib.auth.urls")),
+    path('test/', views.TestPage.as_view(), name="test"),
+    path('thanks/', views.ThanksPage.as_view(), name="thanks"),
+    path('posts/', include("posts.urls", namespace="posts")),
+    path('groups/',include("groups.urls", namespace="groups"))
+]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    import debug_toolbar
+    import mimetypes
+
+    mimetypes.add_type("application/javascript", ".js", True)
+
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls)), ] + urlpatterns
